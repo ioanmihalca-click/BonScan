e0fd616503e9b20331f0bb4a3b8fdde87d8244e3
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-01-30 21:30:02 +0200

    breeze

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,11 @@
 <footer class="py-16 text-sm text-center text-gray-600">
 
-            
+                  <div class="my-4 mb-8">
+                        <p class="font-medium text-black">
+                            ⚠️ Disclaimer: Atât oamenii cât și AI-ul pot face erori.
+                            Vă rugăm verificați datele generate și editați-le dacă este necesar!
+                        </p>
+                    </div>
 
                     <div class="flex items-center justify-center">
                         <img src="assets/logo.webp" alt="BonScan Logo" class="w-10 h-10 mr-2">
@@ -9,7 +14,7 @@
 
                   
                     <div class="mb-2 text-sm text-center">
-                        <span class="text-gray-500">Dezvoltat cu ❤️ de</span>
+                        <span class="text-gray-500">Powered by</span>
                         <a href="https://clickstudios-digital.com"
                             class="font-medium text-indigo-600 hover:text-indigo-500" target="_blank"
                             rel="noopener noreferrer">
@@ -18,10 +23,4 @@
                     </div>
 
                     <div>&copy; {{ date('Y') }} BonScan. Toate drepturile rezervate.</div>
-                          <div class="mt-4">
-                        <p class="font-medium text-yellow-600">
-                            ⚠️ Disclaimer: Atât oamenii cât și AI-ul pot face erori.
-                            Vă rugăm verificați datele generate și editați-le dacă este necesar!
-                        </p>
-                    </div>
                 </footer>
