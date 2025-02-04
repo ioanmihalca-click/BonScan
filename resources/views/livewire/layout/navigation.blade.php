b5c39aab28fbb7424b15813b133f38bf49d4338b
Ioan "Click" Mihalca <164161041+ioanmihalca-click@users.noreply.github.com>
2025-02-04 10:25:30 +0200

    prompt

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -56,14 +56,6 @@ new class extends Component {
                     ])>
                         Situații
                     </a>
-                    {{-- <a href="/bonuri" wire:navigate @class([
-                        'inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2',
-                        'border-indigo-500 text-gray-900' => request()->routeIs('bonuri*'),
-                        'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' => !request()->routeIs(
-                            'bonuri*'),
-                    ])>
-                        Bonuri
-                    </a> --}}
                 </div>
             </div>
 
